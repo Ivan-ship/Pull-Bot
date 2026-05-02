b0178d6388a287cc64609ebc9c8ad466c674a3af
Ivan-ship <vanyfug3@gmail.com>
2026-05-02 13:53:50 +0300

    Реализация проходения опроса

--- handlers/start.py
+++ handlers/start.py
@@ -6,12 +6,17 @@ import aiohttp
 from aiogram import Router
 from dotenv import load_dotenv
 from buttons import main_kb
+from aiogram.fsm.context import FSMContext
+from states import TestState
+from aiohttp import ClientSession
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 load_dotenv()
 
 router = Router()
 API_URL = os.getenv("API_URL")
 API_KEY = os.getenv("API_KEY")
+API_BASE = os.getenv("API_BASE")
 
 #Приветствие бота
 @router.message(CommandStart())
@@ -60,3 +65,86 @@ async def command_start_handler(message: Message):
         await message.answer("Неизвестная ошибка!")
         print(f"Неизвестная ошибка! {e}")
 
+
+#Список тестов
+@router.message(lambda m: m.text == "Пройти опрос")
+async def show_tests(message: Message, state: FSMContext):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{API_BASE}/tests") as resp:
+            tests = await resp.json()
+        
+        kb = []
+        for test in tests:
+            kb.append([KeyboardButton(text=f"{test['test_id']}:{test['title']}")])
+        markup = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+
+        await state.set_state(TestState.choosing_test)
+        await message.answer("Выберите опрос: ", reply_markup=markup)
+
+#Выбор теста
+@router.message(TestState.choosing_test)
+async def select_test(message: Message, state: FSMContext):
+    try:
+        test_id = int(message.text.split(":")[0])
+    except:
+        await message.answer("выберите тест!")
+        return
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{API_BASE}/tests/{test_id}") as resp:
+            test = await resp.json()
+    
+    await state.update_data(
+        test_id=test_id,
+        questions=test["questions"],
+        current_q = 0,
+        answers=[]
+    )
+
+    await state.set_state(TestState.passing_test)
+    await send_question(message, state)
+
+#отправка вопросов
+async def send_question(message: Message, state: FSMContext):
+    data = await state.get_data()
+
+    if data["current_q"] >= len(data["questions"]):
+        await message.answer("Тест завершён!")
+        return
+
+    q = data["questions"][data["current_q"]]
+
+    kb = InlineKeyboardMarkup(
+        inline_keyboard = [
+            [
+                InlineKeyboardButton(
+                    text = ans["text"],
+                    callback_data=f"ans:{q['question_id']}:{ans['answer_id']}"
+                )
+            ]
+            for ans in q['answers']
+        ]
+    )
+    await message.answer(q["text"], reply_markup=kb)
+
+
+#Обработчик кнопки
+@router.callback_query(lambda c: c.data.startswith("ans:"))
+async def handle_answer(callback: CallbackQuery, state: FSMContext):
+    _, question_id, answer_id = callback.data.split(":")
+
+    data = await state.get_data()
+
+    answers = data["answers"]
+    answers.append({
+        "question_id": int(question_id),
+        "answer_id": int(answer_id)
+    })
+
+    await state.update_data(
+        answers = answers,
+        current_q = data["current_q"] + 1
+    )
+
+    await callback.answer("Ответ принят!")
+    await send_question(callback.message, state)
